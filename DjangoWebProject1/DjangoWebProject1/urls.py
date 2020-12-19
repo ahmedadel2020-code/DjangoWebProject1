61f50a809ef5878a137aa80c3ebf4d80e48c3773
ahmedadel2020-code <ahmed.mazen20141415@gmail.com>
2020-12-19 21:09:15 +0200

    fix the issue of naming the app

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 """
 
 from django.conf.urls import include, url
-import HelloDjangoApp.views
+import HealthTracking.views
 
 # Django processes URL patterns in the order they appear in the array
 urlpatterns = [
-    url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^$', HealthTracking.views.index, name='index'),
+    url(r'^home$', HealthTracking.views.index, name='home'),
 ]
